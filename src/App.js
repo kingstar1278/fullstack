745ab3d3e7f4bb3b7576826b52ffec0575f444d8
kingstar1278 <82220514+kingstar1278@users.noreply.github.com>
2021-05-04 17:02:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from'react';
+import './App.css';
+import Contact from './components/contact';
+
+function App() {
+  return (
+   <div className='row'>
+     <div className='col-md-8 offset-md-1'>
+       <Contact/>
+
+     </div>
+
+   </div>
+  );
+}
+
+export default App;
